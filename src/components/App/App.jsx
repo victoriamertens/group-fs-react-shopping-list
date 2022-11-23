de6209da8dabbe5c4eb92da89314cbf22a98af33
Brett R. Gebbie <brg078@gmail.com>
2022-11-23 10:47:45 -0600

    clientget

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
-
+import { useEffect, useState } from 'react';
 import Header from '../Header/Header.jsx'
 import './App.css';
+import axios from 'axios';
+
+
+useEffect(() => {
+    fetchItems();
+  }, [])
+
+
+const fetchItems = () => {                      //INVOKED BY: (useEffect)
+    axios.get('/item')
+      .then(response => {
+        console.log(response.data);
+        setItemList(response.data);
+      }).catch(error => {
+        console.log(error);
+      })
+  }                                             //INVOKES: (GET)(setItemList)
 
 
 function App() {
